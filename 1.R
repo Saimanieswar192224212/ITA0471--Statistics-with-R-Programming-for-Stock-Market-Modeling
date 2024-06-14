e68d1344990114437bbfbe8f651fe7c43c656008
Saimanieswar192224212 <129197481+Saimanieswar192224212@users.noreply.github.com>
2024-06-14 08:38:41 +0530

    Add files via upload

---
+++ 1.R
@@ -0,0 +1,10 @@
+R_version <- R.version.string
+print(paste("R Version: ", R_version))
+
+# Take input from the user
+name <- readline("Enter your name: ")
+age <- readline("Enter your age: ")
+
+# Display the values
+print(paste("Name: ", name))
+print(paste("Age: ", age))
